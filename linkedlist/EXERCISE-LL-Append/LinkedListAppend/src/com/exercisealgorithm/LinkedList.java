08f958ffeda325518d78384bb0ddafa9438b2e66
amir-zarchini <amzarchini@gmail.com>
2023-07-09 12:24:33 +0330

    add exercise linkedlist append closed #2

---
+++ linkedlist/EXERCISE-LL-Append/LinkedListAppend/src/com/exercisealgorithm/LinkedList.java
@@ -0,0 +1,61 @@
+package com.exercisealgorithm;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node node = new Node(value);
+        if (length == 0) {
+            tail = node;
+            head = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        length++;
+    }
+}
---
+++ linkedlist/EXERCISE-LL-Append/LinkedListAppend/src/com/exercisealgorithm/Main.java
@@ -0,0 +1,19 @@
+package com.exercisealgorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+    }
+}
+
