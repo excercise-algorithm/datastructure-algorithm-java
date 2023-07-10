4287935be792cedb0e51da3d8163d4555881c4d8
amir-zarchini <amzarchini@gmail.com>
2023-07-10 11:18:46 +0330

    add linkedlist insert exercise closes #9

---
+++ linkedlist/EXERCISE-LL-Insert/LinkedListInsert/src/com/exercisealgorithm/Main.java
@@ -0,0 +1,28 @@
+package com.exercisealgorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(3);
+
+        System.out.println("LL before insert():");
+        myLinkedList.printList();
+
+        myLinkedList.insert(1, 2);
+
+        System.out.println("\nLL after insert(2) in middle:");
+        myLinkedList.printList();
+
+        myLinkedList.insert(0, 0);
+
+        System.out.println("\nLL after insert(0) at beginning:");
+        myLinkedList.printList();
+
+        myLinkedList.insert(4, 4);
+
+        System.out.println("\nLL after insert(4) at end:");
+        myLinkedList.printList();
+        
+    }
+}
