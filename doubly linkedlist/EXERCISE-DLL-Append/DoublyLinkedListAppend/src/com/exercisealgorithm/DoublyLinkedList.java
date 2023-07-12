d9e7ae7da6337b24186169145caffdca13ce7338
amir-zarchini <amzarchini@gmail.com>
2023-07-12 12:02:54 +0330

    add dll append exercise

---
+++ doubly linkedlist/EXERCISE-DLL-Append/DoublyLinkedListAppend/src/com/exercisealgorithm/DoublyLinkedList.java
@@ -0,0 +1,56 @@
+package com.exercisealgorithm;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("head: null");
+        } else {
+            System.out.println("head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (tail == null) {
+            System.out.println("tail: null");
+        } else {
+            System.out.println("tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("length: " + length);
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+}
