9f29d3aab9656c9b97065a97ea9588fe4e156415
amir-zarchini <amzarchini@gmail.com>
2023-07-12 14:24:15 +0330

    edit dll prepend

--- doubly linkedlist/EXERCISE-DLL-Prepend/DoublyLinkedListPrepend/src/com/exercisealgorithm/DoublyLinkedList.java
+++ doubly linkedlist/EXERCISE-DLL-Prepend/DoublyLinkedListPrepend/src/com/exercisealgorithm/DoublyLinkedList.java
@@ -45,12 +45,11 @@ public class DoublyLinkedList {
         Node newNode = new Node(value);
         if(length == 0) {
             head = newNode;
-            tail = newNode;
         } else {
             tail.next = newNode;
             newNode.prev = tail;
-            tail = newNode;
         }
+        tail = newNode;
         length++;
     }
 
